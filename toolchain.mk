2d1ca3d6e633784ad6eafd42edd07e36b1b32e5e
ferraith <ferraith@gmail.com>
2012-11-17 12:48:26 +0100

    Initial commit

---
+++ toolchain.mk
@@ -0,0 +1,35 @@
+########################################################################################################################
+#
+# Workspace: Android Open Accessory Software for AOAA Kit
+#
+# Description:
+#	Makefile used for the configuration of the toolchain.
+#
+#============================== Paths ==================================================================================
+CW_DIR       := c:/DevTools/CrossWorks_for_ARM_2.2
+CWT_DIR      := $(CW_DIR)/bin
+CWL_DIR      := $(CW_DIR)/lib
+CWI_DIR      := $(CW_DIR)/include
+GCC_DIR      := $(CW_DIR)/gcc/arm-unknown-eabi/bin
+
+#============================== GNU Compiler Collection ================================================================
+CC          := $(GCC_DIR)/cc1.exe
+CPP         := $(GCC_DIR)/cc1plus.exe
+AR          := $(GCC_DIR)/ar.exe
+AS          := $(GCC_DIR)/as.exe
+LD          := $(GCC_DIR)/ld.exe
+OBJCOPY     := $(GCC_DIR)/objcopy.exe
+
+#============================== Rowley Crossworks ======================================================================
+CCD         := $(CWT_DIR)/cc.exe
+CROSSSCRIPT := $(CWT_DIR)/crossscript.exe
+
+#============================== Binutils ===============================================================================
+CP          := cp
+ECHO        := echo -e
+MAKE        := make -s
+MKDIR       := mkdir
+RM          := rm -f
+
+#============================== Unix Utilities =========================================================================
+SED         := sed
