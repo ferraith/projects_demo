b064049b2e03d1fd0a9bcf927c20b913a040bd68
ferraith <ferraith@gmail.com>
2012-12-27 17:59:53 +0100

    Introduced class_helper header file

--- src/console.h
+++ src/console.h
@@ -4,8 +4,10 @@
 #ifndef CONSOLE_H_
 #define CONSOLE_H_
 
+#include "stdint.h"
+
+#include "class_helper.h"
 #include "lpc17xx_uart.h"
-#include "types.h"
 
 ///
 /// @brief         Provides methods to send and receive data to or from a console connected via UART to a
