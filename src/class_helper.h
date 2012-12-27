2c233a4d663e87736ba322412d198b062d67937a
ferraith <ferraith@gmail.com>
2012-12-27 17:48:38 +0100

    Squashed 'platform/common/' content from commit b6b4b44
    git-subtree-dir: platform/common
    git-subtree-split: b6b4b446c6d84bf0a3ec50a9baeaad0115c37949

---
+++ src/class_helper.h
@@ -0,0 +1,17 @@
+// Common Source Files
+// Copyright (C) ferraith. All rights reserved.
+
+#ifndef CLASS_HELPER_H_
+#define CLASS_HELPER_H_
+
+///
+/// @brief         A macro to disallow the copy constructor and operator= function. This should be used in the private
+///                declarations for a class
+/// @param[in]     class_name  The name of the class
+/// @return        None
+///
+#define DISALLOW_COPY_AND_ASSIGN(class_name) \
+  class_name(const class_name&);             \
+  void operator=(const class_name&)
+
+#endif  // CLASS_HELPER_H_
