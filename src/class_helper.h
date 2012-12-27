1e155106f4e8ba4c65d30dc6b0f98be8f68b371e
ferraith <ferraith@gmail.com>
2012-12-27 15:07:38 +0100

    Added class_helper header

---
+++ src/class_helper.h
@@ -0,0 +1,17 @@
+// Demo Application
+// Copyright (C) ferraith. All rights reserved.
+
+#ifndef TYPES_H_
+#define TYPES_H_
+
+///
+/// @brief         A macro to disallow the copy constructor and operator= function. This should be used in the private
+///                declarations for a class
+/// @param[in]     class_name The name of the class
+/// @return        None
+///
+#define DISALLOW_COPY_AND_ASSIGN(class_name) \
+  class_name(const class_name&);             \
+  void operator=(const class_name&)
+
+#endif  // TYPES_H_
