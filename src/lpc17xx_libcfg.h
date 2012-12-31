02320d66492ee3fd2f3c52ef906b5fab50cc71e8
ferraith <ferraith@gmail.com>
2012-12-31 14:16:38 +0100

    Reordered file structure to fit Google C++ Style conventions

--- src/lpc17xx_libcfg.h
+++ src/lpc17xx_libcfg.h
@@ -1,9 +1,5 @@
+// LPC17xx Library
 // Copyright (C) ferraith. All rights reserved.
-
-///
-/// @brief LPC17xx library configuration file
-/// @author ferraith
-///
 
 #ifndef LPC17XX_LIBCFG_H_
 #define LPC17XX_LIBCFG_H_
@@ -11,39 +7,39 @@
 #include "lpc_types.h"
 
 // Comment the line below to disable the specific peripheral inclusion
-#define _DBGFWK  ///< DEBUG_FRAMWORK
-#define _GPIO    ///< GPIO
-#define _EXTI    ///< EXTI
-#define _UART    ///< UART
-#define _UART0   ///< UART0
-#define _UART1   ///< UART1
-#define _UART2   ///< UART2
-#define _UART3   ///< UART3
-#define _SPI     ///< SPI
-#define _SYSTICK ///< SYSTICK
-#define _SSP     ///< SSP
-#define _SSP0    ///< SSP0
-#define _SSP1    ///< SSP1
-#define _I2C     ///< I2C
-#define _I2C0    ///< I2C0
-#define _I2C1    ///< I2C1
-#define _I2C2    ///< I2C2
-#define _TIM     ///< TIMER
-#define _WDT     ///< WDT
-#define _GPDMA   ///< GPDMA
-#define _DAC     ///< DAC
-#define _ADC     ///< ADC
-#define _PWM     ///< PWM
-#define _PWM1    ///< PWM1
-#define _RTC     ///< RTC
-#define _I2S     ///< I2S
-#define _USBDEV  ///< USB device
-#define _USB_DMA ///< USB DMA
-#define _QEI     ///< QEI
-#define _MCPWM   ///< MCPWM
-#define _CAN     ///< CAN
-#define _RIT     ///< RIT
-#define _EMAC    ///< EMAC
+#define _DBGFWK   ///< DEBUG_FRAMWORK
+#define _GPIO     ///< GPIO
+#define _EXTI     ///< EXTI
+#define _UART     ///< UART
+#define _UART0    ///< UART0
+#define _UART1    ///< UART1
+#define _UART2    ///< UART2
+#define _UART3    ///< UART3
+#define _SPI      ///< SPI
+#define _SYSTICK  ///< SYSTICK
+#define _SSP      ///< SSP
+#define _SSP0     ///< SSP0
+#define _SSP1     ///< SSP1
+#define _I2C      ///< I2C
+#define _I2C0     ///< I2C0
+#define _I2C1     ///< I2C1
+#define _I2C2     ///< I2C2
+#define _TIM      ///< TIMER
+#define _WDT      ///< WDT
+#define _GPDMA    ///< GPDMA
+#define _DAC      ///< DAC
+#define _ADC      ///< ADC
+#define _PWM      ///< PWM
+#define _PWM1     ///< PWM1
+#define _RTC      ///< RTC
+#define _I2S      ///< I2S
+#define _USBDEV   ///< USB device
+#define _USB_DMA  ///< USB DMA
+#define _QEI      ///< QEI
+#define _MCPWM    ///< MCPWM
+#define _CAN      ///< CAN
+#define _RIT      ///< RIT
+#define _EMAC     ///< EMAC
 
 
 ///
@@ -56,7 +52,7 @@
 /// @return     None
 ///
 #ifdef  DEBUG
-#define CHECK_PARAM(expr) ((expr) ? (void)0 : check_failed((uint8_t *)__FILE__, __LINE__))
+#define CHECK_PARAM(expr) ((expr) ? (void) 0 : check_failed((uint8_t *)__FILE__, __LINE__))
 #else
 #define CHECK_PARAM(expr)
 #endif  // DEBUG
