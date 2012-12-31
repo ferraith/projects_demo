80a36d09daa9124c055dc9f8f0a9536deea1d137
ferraith <ferraith@gmail.com>
2012-12-31 14:16:38 +0100

    Reordered file structure to fit Google C++ Style conventions

--- src/lpc17xx_libcfg.c
+++ src/lpc17xx_libcfg.c
@@ -1,18 +1,14 @@
+// LPC17xx Library
 // Copyright (C) ferraith. All rights reserved.
-
-///
-/// @brief LPC17xx library configuration file
-/// @author ferraith
-///
 
 #include "lpc17xx_libcfg.h"
 
 #ifdef  DEBUG
 void check_failed(uint8_t *file, uint32_t line)
 {
-    // User can add his own implementation to report the file name and line number,
-	// ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line)
+  // User can add his own implementation to report the file name and line number.
+  // Example: printf("Wrong parameters value: file %s on line %d\r\n", file, line)
 
-    while(1);
+    while (1);
 }
 #endif  // DEBUG
